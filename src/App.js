fa96449e7901237d2eeb11cf3ce33f42b80dc283
matheus <freitas1matheus@gmail.com>
2022-08-13 20:00:57 -0300

    CSS to SASS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Today from './components/Today';
 
 
 function App() {
@@ -34,9 +33,30 @@ function App() {
             <h1>About Today</h1>
             <h5>Welcome! Today is {today.api_response.date} </h5>
         </header>
-      <Today />
+
+        <section class="history">
+        <a name="events"><h1 id="today">Today in History</h1></a>
+        <div>
+          <a id="learn-more" href={today.api_response.url}>learn more on Wikipedia</a>
+        </div>          
+        
+        <div id="ticker">
+          <h2>Events</h2>
+          <div>
+            <ul id="events" class="ticker"></ul>
+          </div>
+          <div>
+            <h2>Births</h2>
+            <ul id="births" class="ticker"></ul>
+          </div>
+          <div>
+            <h2>Deaths</h2>
+            <ul id="deaths" class="ticker"></ul>
+          </div>             
+        </div>
+      </section>
       
-      {today.api_response.url}
+      
       
       
     </div>
