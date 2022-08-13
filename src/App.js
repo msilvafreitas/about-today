0e96897e46037429e4853b907cb50034e5293c40
matheus <freitas1matheus@gmail.com>
2022-08-13 19:26:30 -0300

    API integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Today from './components/Today';
+
 
 function App() {
+
+
+        const [today, setToday] = useState({
+          api_response: [], 
+          loading: true,
+        });
+
+        let api_url = `https://history.muffinlabs.com/date`;
+      
+        const fetchData = () => {
+          fetch(api_url)
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              setToday({ ...today, api_response: data, loading: false });
+            });
+        };
+
+        useEffect(() => {
+          fetchData();
+        }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='App-header'>
+            <h1>About Today</h1>
+            <h5>Welcome! Today is {today.api_response.date} </h5>
+        </header>
+      <Today />
+      
+      {today.api_response.url}
+      
+      
     </div>
   );
 }
