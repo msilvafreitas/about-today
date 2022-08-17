f536e5e78a584f429ac646f6768bf30f50c17f0b
matheus <freitas1matheus@gmail.com>
2022-08-17 17:25:31 -0300

    Revert "Api data insert"
    This reverts commit e2f72c8de3c7972a75d161bfeeea2dfa5eac5095.

--- src/App.js
+++ src/App.js
@@ -44,37 +44,19 @@ function App() {
           <div class="today">
           <h2>Historical Events</h2><button onClick={fetchData()}>Show/Hide</button>
             <ul id="events">
-            {
-                today.api_response.data.Events.map(event=>{
-                  return(<li>
-                    <p>{event.year} - {event.text}</p>
-                  </li>)
-                })
-              }
+            
             </ul>
           </div>
           <div class="today">
             <h2>Births</h2><button>Show/Hide</button>
             <ul id="births">
-            {
-                today.api_response.data.Births.map(event=>{
-                  return(<li>
-                    <p>{event.year} - {event.text}</p>
-                  </li>)
-                })
-              }
+            
             </ul>
           </div>
           <div class="today">
             <h2>Deaths</h2><button>Show/Hide</button>
             <ul id="deaths">
-            {
-                today.api_response.data.Deaths.map(event=>{
-                  return(<li>
-                    <p>{event.year} - {event.text}</p>
-                  </li>)
-                })
-              }
+            
             </ul>
           </div>             
         </div>
